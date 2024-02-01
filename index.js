92a379a26bde67c40345fa89b4aaee6f1d672504
jimmyroot <jimmybnewman@gmail.com>
2024-02-01 17:50:28 +0000

    CSS changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // TODO: Fix button states
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js'
-import { getDatabase, ref, onValue, push, remove } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js'
+import { getDatabase, ref, onValue, push, remove, child, get } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js'
 // import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 
 // Init firebase
@@ -18,17 +18,19 @@ const app = initializeApp(cfg)
 const db = getDatabase(app)
 const tasks = ref(db, 'tasks')
 const invoices = ref(db, 'invoices')
+const userPrefs = ref(db, 'userPrefs')
 
 // Grab the DOM elements we need
 const formTaskInput = document.querySelectorAll('#frm-task-input')[0]
 const ulTasks = document.getElementById('ul-invoice-tasks')
 const btnSendInvoice = document.getElementById('btn-send-invoice')
+const btnReset = document.getElementById('btn-reset')
 const modalHistory = document.getElementById('modal-invoice-history')
 
 // We'll maintain a local array of current tasks, so we can check if tasks already exists.
 // This means we don't need to check the db every time we add a new task
 let liveTasks = []
-let newInvoiceTotal = 0
+let invoiceTotal = 0
 
 // ----------------------- //
 // --- EVENT LISTENERS --- //
@@ -37,12 +39,14 @@ let newInvoiceTotal = 0
 formTaskInput.addEventListener('submit', e => {
     e.preventDefault()
     const form = e.target
+    // Check if task input field contains something, then submit the form
     if (isFormComplete(form)) handleFormSubmit(form)
 })
 
 formTaskInput.addEventListener('input', e => {        
     const input = e.target
     if (input.classList.contains('warning')) input.classList.remove('warning')
+    if (input.value) enableButtons([btnReset], true)
 })
 
 ulTasks.addEventListener('click', e => {
@@ -66,7 +70,7 @@ document.getElementById('div-ctl-panel').addEventListener('click', e => {
             showModal(modalHistory, true)
         },
         reset: () => {
-
+            
         }
     }
 
@@ -78,16 +82,24 @@ modalHistory.addEventListener('click', e => {
     const handleClick = {
         close: () => {
             showModal(modalHistory, false)
+        },
+        invoiceDelete: () => {
+            removeInvoiceFromDB(e.target.dataset.key)
         }
     }
     const type = e.target.dataset.type
     if (type) handleClick[type]()
 })
 
+document.getElementById('btn-reset').addEventListener('click', () => {
+    resetInvoice()
+})
+
 // ----------------------- //
 // --- EVENT HANDLERS  --- //
 // ------------------—---- //
 
+// Here is where we add the task
 const handleFormSubmit = form => {
     console.log(form)
     const data = new FormData(form)
@@ -97,38 +109,54 @@ const handleFormSubmit = form => {
         price: +(data.get('price'))
     }
     
-    // Object.fromEntries(new FormData(form))
-    if (!taskExists(task)) pushTaskToDB(task)
+    // Make sure there's no duplicate then push
+    if (!taskExists(task)) push(tasks, task)
+    formTaskInput.reset()
 }
 
+// Called when send invoice button clicked, create an invoice obj and store it in fb
 const handleSendInvoice = () => {
-    const invoice = {
-        tasks: [],
-        total: 0,
-        date: [date],
-        
+
+    // I guess we could do some checking here to prevent submission of duplicate invoices, but leaving that alone for now :-)
+    const newInvoiceTasks = liveTasks.map(task => task[0])
+    const newInvoiceTotal = invoiceTotal
+    const newInvoiceDate = new Date().toLocaleDateString('en-GB', { month: 'short', day: 'numeric' })
+
+    const newInvoice = {
+        tasks: newInvoiceTasks,
+        total: newInvoiceTotal,
+        date: newInvoiceDate
     }
+
+    push(invoices, newInvoice)
+    resetInvoice()
+    // Show a modal
 }
 
 // --------------------—----- //
 // --- FIREBASE FUNCTIONS --- //
 // ------------------—------- //
 
+// Render the task list 
 onValue(tasks, snapshot => {
     snapshot.exists() ? renderTasks(snapshot.val()) : noTasksYet()
 })
 
-const pushTaskToDB = task => {
-    push(tasks, task)
-}
+// Toggle dark mode when darkmode value changed
+onValue(userPrefs, snapshot => {
+    if (snapshot.exists()) console.log('dark mode toggle')
+})
 
-const pushInvoiceToDB = invoice => {
-    
-}
-
+// Delete a task from db
 const removeTaskFromDB = key => {
     const taskRef = ref(db, `tasks/${key}`)
     remove(taskRef)
+}
+
+const removeInvoiceFromDB = key => {
+    const invoiceRef = ref(db, `invoices/${key}`)
+    remove(invoiceRef)
+    renderInvoiceHistory()
 }
 
 // ------------------------ //
@@ -137,12 +165,12 @@ const removeTaskFromDB = key => {
 
 const renderTasks = tasks => {
     liveTasks = []
-    newInvoiceTotal = 0
+    invoiceTotal = 0
 
     const html = Object.keys(tasks).map(key => {
         const { name, price } = tasks[key]
         liveTasks.push([name, price]) // Keep local tasklist in sync with DB
-        newInvoiceTotal += price
+        invoiceTotal += price
         return `
             <li class="li-invoice-task">
                 <p>${name}</p>
@@ -153,20 +181,48 @@ const renderTasks = tasks => {
     }).join('')
 
     ulTasks.innerHTML = html
-    renderNewInvoiceTotal()
-    
+   
+    enableButtons([btnSendInvoice, btnReset], true)
+    renderinvoiceTotal()
 }
 
+// Using 'get' to perform a single read of the invoices data, preventing it from rendering in the background
+// if we use 'onValue'. Called when modal displayed, or when an invoices is removed from the db
 const renderInvoiceHistory = () => {
-
+    const ulInvoiceHistory = document.getElementById('ul-invoice-history')
+    get(invoices).then(snapshot => {
+        if (snapshot.exists()) {
+            const invoiceHistory = snapshot.val()
+            ulInvoiceHistory.innerHTML = Object.keys(invoiceHistory).map(key => {
+                const { date, tasks, total } = invoiceHistory[key]
+                return `
+                    <li>
+                        <p>Invoice Date: ${date}</p>
+                        <p>Tasks done: ${
+                            tasks.map(task => task).join(', ')
+                        }</p>
+                        <p>Invoice amount: ${total} </p>
+                        <button data-type="invoiceDelete" data-key=${key}>Delete</button>
+                    </li>
+                `
+            }).join('')
+        } else {
+            ulInvoiceHistory.innerHTML = `
+                <li>
+                    <p>No history...you haven't done much work!</p>
+                </li>
+            `
+        }
+    }).catch(error => {
+        console.error(error)
+    })
 }
 
 const showModal = (modal, doShow) => {
-    
     // These functions will be called before the modal displays
     const getReadyToShow = {
         'modal-invoice-history': () => {
-            console.log('rendering history')
+            renderInvoiceHistory()
         }
     }
     
@@ -198,20 +254,28 @@ const noTasksYet = () => {
             <p class="p-task-price">£0</p>
         </li>
     `
-    renderNewInvoiceTotal()
+    enableButtons([btnSendInvoice, btnReset], false)
+    renderinvoiceTotal()
 }
 
-// const getInvoiceTotal = liveTasks => {
-//     if (liveTasks.length > 0) {
-//         const totalDue = liveTasks.reduce((total, task) => total + task[1], 0)
-//         return totalDue
-//     } else {
-//         return `0`
-//     }
-// }
+const resetInvoice = () => {
+    // Reset all the stuff
+    
+    invoiceTotal = 0
+    renderinvoiceTotal()    
+    // Remove current tasks from firebase, will trigger re-render
+    remove(tasks)
+    // Reset form and remove warning class if present
+    formTaskInput.reset()
+    const formEls = [...formTaskInput.elements]
+    formEls.forEach(el => {
+        if (el.classList.contains('warning')) el.classList.remove('warning')
+    })
+    enableButtons([btnReset], false)
+}
 
-const renderNewInvoiceTotal = () => {
-    document.getElementById('p-invoice-footer-total').innerHTML = `£${newInvoiceTotal}`
+const renderinvoiceTotal = () => {
+    document.getElementById('p-invoice-footer-total').innerHTML = `£${invoiceTotal}`
 }
 
 const taskExists = task => {
@@ -228,6 +292,10 @@ const isFormComplete = form => {
     }
     // If there were no empty inputs return true
     return true
+}
+
+const enableButtons = (buttons, doEnable) => {
+    if (buttons.length > 0) buttons.forEach(button => button.disabled = !doEnable)
 }
 
 // Showtime
